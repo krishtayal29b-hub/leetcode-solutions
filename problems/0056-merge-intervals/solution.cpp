0ca5e4d484b47b0bfd9045d269a1061916a6dee9
Krish <krishtayal29b@gmail.com>
2026-07-20 19:03:06 +0530

    Solved: Merge Intervals (#56)

--- problems/0056-merge-intervals/solution.cpp
+++ problems/0056-merge-intervals/solution.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
            if(ans.empty() || intervals[i][0] > ans.back()[1]){
-            ans.push_back(arr[i]);
+            ans.push_back(intervals[i]);
            }
            else{
             ans.back()[1] = max(intervals[i][1] , ans.back()[1]);
