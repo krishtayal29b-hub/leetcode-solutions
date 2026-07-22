924f65f53bae39d9c9ebed4caba9fd5a9da1cd4f
Krish <krishtayal29b@gmail.com>
2026-07-22 12:23:12 +0530

    Solved: 3Sum (#15)

--- problems/0015-3sum/solution.cpp
+++ problems/0015-3sum/solution.cpp
@@ -17,17 +17,46 @@ public:
                 else{
                 vector<int> temp={nums[i],nums[j],nums[k]};
                 ans.push_back(temp);
-
                     k--;
                     j++;
-                while(j<k && nums[j-1] == nums[j]) continue;
-                while(j<k && nums[k+1] == nums[k]) continue;
                 }
-                // while(j<k && nums[j-1] == nums[j]) continue;
-                // while(j<k && nums[k+1] == nums[k]) continue;
+                if(j>i+1)
+                while(j<k && nums[j-1] == nums[j]) j++;
+                if(k<n-1)
+                while(j<k && nums[k+1] == nums[k]) k--;
             }
                 
             }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>> ans;
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<n;i++){
+//             if(i>0 && nums[i]==nums[i-1])  continue;
+//         int j=i+1;
+//         int k=n-1;
+//            while(j<k) {
+//             int sum=nums[i]+nums[j]+nums[k];
+//             if(sum<0) j++;
+//             else if(sum>0) k--;
+//             else{
+//                 vector<int> temp={nums[i],nums[j],nums[k]};
+//                 ans.push_back(temp);
+//                  j++;
+//                  k--;
+//             }
+//             if(j>i+1)
+//             while(j<k && nums[j]==nums[j-1]) j++;
+//             if(k<n-1)
+//             while(j<k && nums[k]==nums[k+1]) k--;
+//            }
+//         }
+//         return ans;
+//     }
+// };
