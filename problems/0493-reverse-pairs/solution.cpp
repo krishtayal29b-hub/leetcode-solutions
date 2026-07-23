53ee2eae903b9f54cec573e2032b1066c50cab5d
Krish <krishtayal29b@gmail.com>
2026-07-23 23:59:42 +0530

    Solved: Reverse Pairs (#493)

--- problems/0493-reverse-pairs/solution.cpp
+++ problems/0493-reverse-pairs/solution.cpp
@@ -7,7 +7,7 @@ public:
             while((arr[i] > arr[right]*2) && right <= high){
                 right++;
             } 
-            cnt += (right - mid + 1);
+            cnt += (right - (mid + 1));
         }
     }
 
@@ -15,7 +15,7 @@ public:
         vector<int> temp;
         int n = arr.size();
         int left = low, right = mid+1;
-        while(left <= mid && right <= high){
+        while(right <= high && left <= mid){
             if(arr[left] >= arr[right]){
                 temp.push_back(arr[right]);
                 right++;
@@ -35,7 +35,7 @@ public:
             right++;   
         }
 
-        for(int i = 0;i<n;i++){
+        for(int i = low;i<high ;i++){
             arr[i] = temp[i - low];
         }
     }
