5937bdc051e03f5bf052593c853eadd7f40989a3
Krish <krishtayal29b@gmail.com>
2026-07-23 23:55:40 +0530

    Solved: Reverse Pairs (#493)

---
+++ problems/0493-reverse-pairs/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+        int cnt = 0;
+    void reversepairs(vector<int>& arr, int low, int mid, int high, int n){
+        int right = mid + 1;
+        for(int i = 0;i <= mid; i++){
+            while((arr[i] > arr[right]*2) && right <= high){
+                right++;
+            } 
+            cnt += (right - mid + 1);
+        }
+    }
+
+    void merge(vector<int>& arr, int low, int mid, int high){
+        vector<int> temp;
+        int n = arr.size();
+        int left = low, right = mid+1;
+        while(left <= mid && right <= high){
+            if(arr[left] >= arr[right]){
+                temp.push_back(arr[right]);
+                right++;
+            }
+            else{
+                temp.push_back(arr[left]);
+                left++;
+            }
+        }
+        while(left <= mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while(right <= high){
+            temp.push_back(arr[right]);
+            right++;   
+        }
+
+        for(int i = 0;i<n;i++){
+            arr[i] = temp[i - low];
+        }
+    }
+
+    void mergesort(vector<int>& arr, int low, int high){
+        if(low >= high) return;
+        int mid = (low + high)/2;
+        mergesort(arr,low, mid);
+        mergesort(arr, mid+1, high);
+        int n = arr.size();
+        reversepairs(arr, low, mid, high, n);
+        merge(arr, low, mid, high);
+    }
+
+    int reversePairs(vector<int>& arr) {
+        int n = arr.size();
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i+1; j<n;j++){
+        //         if(nums[i] > nums[j]*2) cnt++;
+        //     }
+        // }
+        // return cnt;
+        int low = 0;
+        int high = n-1;
+        mergesort(arr, low, high);
+        return cnt;
+    }
+};
